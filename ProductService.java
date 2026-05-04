2276fbeb8e0f91bc9a9891a0b4d363ca9a71d889
Ranim Ourari <127159324+RanimOurari@users.noreply.github.com>
2026-05-04 22:31:59 +0200

    Add ProductService class to manage products

---
+++ ProductService.java
@@ -0,0 +1,16 @@
+package ch.unifr.shopcli;
+
+import java.util.List;
+
+public class ProductService {
+
+    private final ProductRepository repository;
+
+    public ProductService(ProductRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Product> getAllProducts() {
+        return repository.findAll();
+    }
+}
